ea204a54d428ad85c1e651916fcc67d923d0aeff
UmerovEmran <99113996+UmerovEmran@users.noreply.github.com>
2022-03-06 10:40:07 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,35 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
+  bool cr;
+  int sch = 0;
+  while (true) {
+    int last = size - 1;
+    int start = 0;
+    cr = true;
+    while (start < last) {
+      int pol = (last - start) / 2 + start;
+      if (arr[pol] == value) {
+        for (int i = pol; i < size; i++) {
+          arr[i] = arr[i + 1];
+        }
+        sch += 1;
+        size -= 1;
+        cr = false;
+        break;
+      } else if (arr[pol] > value) {
+          last = pol;
+      } else {
+          start = pol;
+      }
+    }
+    if (start == last && arr[start] == value) {
+      sch += 1;
+    }
+    if (cr) {
+      break;
+    }
+  }
+  return sch;
   return 0; // если ничего не найдено
 }
